95ae22a19aa71a143255e261f0852aafb8e6fa98
ArthurSousa460 <arthurprogrammer460@gmail.com>
2023-09-15 15:24:01 -0300

    update script ande readme

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-# Triangle-is-real
-Código que verifica se os tamanhos informados podem forma um triângulo.
-# Tecnologias
--Python.
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Real Triangle</h1>
+    <p>Script que valida um triângulo e define o tipo desse triângulo:</p>
+    <ul>
+        <li>Equilátero</li>
+        <li>Escaleno</li>
+        <li>Isoceles</li>
+    </ul>
+    <h2>O Código</h2>
+    <p>
+        O código se utiliza de funções para verificar se é possivel criar um triângulo. Para validar o triângulo é <br>necessário satisfazer a seguintes condições.
+    </p>
+    <span>Sendo os lados do triângulos representados pela letras a, b e c. So é considerado triângulo se:</span>
+    <ul>
+        <li>a + b > c;</li>
+        <li>a + c > b;</li>
+        <li>b + c > a;</li>
+    </ul>
+    <p>
+        Para definir o tipo do triângulo é usado a seguinte lógica:
+    </p>
+    <ul>
+        <li>Equilátero - todos os lado iguais;</li>
+        <li>Escaleno - os três lados diferentes;</li>
+        <li>Isoceles - somente dois lados iguais.</li>
+    </ul>
+    <h2>Tecnologias usadas</h2>
+    <ul>
+        <li>Python</li>
+    </ul>
+</body>
+</html>
--- Real_triangle.py
+++ Real_triangle.py
@@ -1,22 +1,50 @@
-def is_triangle(a, b, c):
-    if a > b + c:
-        print('No')
-    elif b > a + c:
-        print('No')
-    elif c > b + a:
-        print('No')
-    else:
-        print('Yes')
+def verify_triangle(a, b, c):
+    if (a + b) > c:
+        if (a + c) > b:
+            if (c + b) > a:
+                return True
+    return False
 
 
-def input_triangle():
-    print('-' * 20, end=' ')
-    print('Real triangle', end=' ')
-    print('-' * 20)
-    a = int(input('Enter the size of side a:'))
-    b = int(input('Enter the size of side b:'))
-    c = int(input('Enter the size of side c:'))
-    is_triangle(a, b, c)
+def verify_equilateral(a, b, c):
+    if verify_triangle(a, b, c) == True:
+        if a == b and a == c and c == b:
+            return True
+    return False
+        
+
+def verify_isosceles(a, b, c ):
+    if verify_triangle(a, b, c) == True:
+        if a == b and c != a:
+            return True
+        elif a == c and b != a:
+            return True
+        elif b == c and a != b:
+            return True
+    return False
 
 
-input_triangle()
+def verify_escaleno(a, b, c):
+    if verify_triangle(a, b, c) == True:
+        if a != b and a != c and c != b:
+            return True
+    return False
+
+
+print("-----------Triangle------------")
+print("Digite o valores dos lados para saber se o triângulo pode ser um e seu tipo")
+a = float(input("Lado A:"))
+b = float(input("Lado B:"))
+c = float(input("Lado C:"))
+equilateral = verify_equilateral(a, b, c)
+escaleno = verify_escaleno(a, b, c)
+isoceles = verify_isosceles(a, b, c)
+if equilateral == True:
+    print('O trângulo digitado é equilátero')
+elif escaleno == True:
+    print('O trângulo digitado é escaleno')
+elif isoceles == True:
+    print('O trângulo digitado é isoceles')
+else: 
+    print('dados inválidos')
+
